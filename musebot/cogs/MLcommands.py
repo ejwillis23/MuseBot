5f93d53ce2987553a4617c8b15816a968654ebbf
ejwillis23 <ejwillis@mail.csuchico.edu>
2023-04-18 13:57:55 -0700

    started multiple people

--- musebot/cogs/MLcommands.py
+++ musebot/cogs/MLcommands.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import urlencode
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -82,163 +83,87 @@ class MLCommands(commands.Cog):
     
         waiting = "Reccomendations for " + song[2] + ":\n"
         await message.edit(content= waiting + await makeRec(amount, song, rec))
-    
-    # @commands.command()
-    # async def dataTest(self, ctx, amount=1, *, songIn):
-    #     message = await ctx.send("Waiting for results...")
-
-    #     cache_handler = spotipy.cache_handler.CacheFileHandler(username=ctx.author)
-    #     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-    #     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-    #         print("Not signed in")
-    #         role = discord.utils.find(lambda m: m.name == 'Users', ctx.guild.roles)
-    #         if role not in ctx.author.roles:
-    #             await ctx.author.add_roles(role)
-
-    #         sign_in(ctx.author)
-    #     sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    #     # song = await songSearch(sp, songIn)
-    #     song = await playlistSearch(sp, songIn)
-    #     rec = dataUse[dataUse.track_name.str.lower() != songIn.lower()]
-    #     waiting = "Waiting for results of " + song[2] + " by " + song[1] + "..."
-    #     await message.edit(content=waiting)
-    #     # waiting = "Waiting for results of " + song[2] + " by " + song[1] + "..."
-    #     # await message.edit(content=waiting)
-    
-    #     await message.edit(content= await makeRec(amount, song, rec))
-
-    # @commands.command()
-    # async def checking(self, ctx, *, songIn):
-    #     cache_handler = spotipy.cache_handler.CacheFileHandler(username=ctx.author)
-    #     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-    #     if not auth_manager.validate_token(cache_handler.get_cached_token()):
-    #         print("Not signed in")
-    #         role = discord.utils.find(lambda m: m.name == 'Users', ctx.guild.roles)
-    #         if role not in ctx.author.roles:
-    #             await ctx.author.add_roles(role)
-
-    #         sign_in(ctx.author)
-    #     sp = spotipy.Spotify(auth_manager=auth_manager)
-
-    #     # song = dataUse[(dataUse.name.str.lower() == songIn.lower())].info()
-    #     # print(song)
-
-    #     # if dataUse.isin([songIn]).any().any():
-    #     #     print("Is in")
-    #     #     song = dataUse[(dataUse.track_name.str.lower() == songIn.lower())].to_json()
-    #     #     print(song)
-    #     # else:
-    #     #     print("Is out")
-    #     #     print(await songSearch(ctx.author, songIn))
-        
-    #     # print(dataUse[(dataUse.name.str.lower() == songIn.lower())].head(1))
-    #     # print(await songSearch(ctx.author, songIn))
-
-    #     # try:
-    #     #     search1 = sp.search(q=songIn, type='track', limit=1)['tracks']['items'][0]['id']
-    #     #     search2 = sp.search(q="Carol of the Bells", type='track', limit=1)['tracks']['items'][0]['id']
-    #     #     recco = sp.recommendations(seed_tracks=[search1, search2], limit=10)
-    #     #     for idx in range(10):
-    #     #         print(recco['tracks'][idx]['name'])
-    #     # except:
-    #     #     print("Error")
-
-    #     try:
-    #         playlists = sp.current_user_playlists(limit=26)
-    #         for items in playlists['items']:
-    #             if songIn.lower() in items['name'].lower():
-    #                 songs = sp.playlist(items['id'], fields='name,id,tracks.items(track(name,id,popularity,artists(name)))')
-    #                 print("Found")
-    #                 break
-    #             # print(items['name'])
-    #         # print(playlists)
-    #         meanslist = ["", "", songs['name'], songs['id'], 0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    #         for idx, song in enumerate(songs['tracks']['items']):
-    #             # print(song)
-    #             audio = sp.audio_features(song['track']['id'])[0]
-    #             meanslist[4] += song['track']['popularity']
-    #             meanslist[5] += audio['acousticness']
-    #             meanslist[6] += audio['danceability']
-    #             meanslist[7] += audio['duration_ms']
-    #             meanslist[8] += audio['energy']
-    #             meanslist[9] += audio['instrumentalness']
-    #             meanslist[10] += audio['key']
-    #             meanslist[11] += audio['liveness']
-    #             meanslist[12] += audio['loudness']
-    #             meanslist[13] += audio['mode']
-    #             meanslist[14] += audio['speechiness']
-    #             meanslist[15] += audio['tempo']
-    #             meanslist[16] += audio['time_signature']
-    #             meanslist[17] += audio['valence']
-    #         songslen = len(songs['tracks']['items'])
-    #         means = ["", meanslist[1], meanslist[2], meanslist[3]]
-    #         for item in meanslist[4:]:
-    #             # print("here")
-    #             means.append(item / songslen)
-    #         print(means)
-    #         # testframe = pd.json_normalize(songs['tracks']['items'])
-    #         # temp = testframe.loc[:, 'track.duration_ms'].mean()
-    #         # print(temp)
-    #     except:
-    #         print("Error")
-
-    #     message = await ctx.send("done")
-    #     await message.add_reaction("🇦")
 
     @commands.command()
-    async def checking(self, ctx, *, songIn):
-        inpSong, inpArtist = songIn.split(" by ") if " by " in songIn else (songIn, "")
-        # print(inpSong, inpArtist)
-        message = await ctx.send("Waiting for results...")
+    async def checking(self, ctx: commands.Context, *username: discord.Member):
+        msg = ""
+        for user in username:
+            # msg += user.mention + " "
+            msg += "@" + user.display_name + " "
+        await ctx.send(f"{msg}: Input your song")
 
-        cache_handler = spotipy.cache_handler.CacheFileHandler(username=ctx.author)
-        auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
-        if not auth_manager.validate_token(cache_handler.get_cached_token()):
-            print("Not signed in")
-            role = discord.utils.find(lambda m: m.name == 'Users', ctx.guild.roles)
-            if role not in ctx.author.roles:
-                await ctx.author.add_roles(role)
+        def check(m: discord.Message):
+            return m.channel == ctx.channel and m.author in username # m.content == "hello"
 
-            sign_in(ctx.author)
-        sp = spotipy.Spotify(auth_manager=auth_manager)
+        songs = []
+        for i in username:
+            msg = await self.client.wait_for("message", check=check)
+            # sp = await sign_in(self.client.get_context)
+            context = await self.client.get_context(msg)
+            cache_handler = spotipy.cache_handler.CacheFileHandler(username=context.author)
+            auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+            if not auth_manager.validate_token(cache_handler.get_cached_token()):
+                print("Not signed in")
+                role = discord.utils.find(lambda m: m.name == 'Users', context.guild.roles)
+                if role not in context.author.roles:
+                    await context.author.add_roles(role)
 
-        song = await songSearch(sp, inpSong, inpArtist)
-        waiting = "Waiting for results of " + song[2] + " by " + song[1] + "...\n"
-        await message.edit(content=waiting)
+                sign_in(context.author)
+            sp = spotipy.Spotify(auth_manager=auth_manager)
+            
+            inpSong, inpArtist = msg.content.split(" by ") if " by " in msg.content else (msg.content, "")
+            song = await songSearch(sp, inpSong, inpArtist)
+            if len(song) == 0:
+                await ctx.send(f"Couldn't find {inpSong}")
+            else: 
+                songs.append(song)
+                await ctx.send(f"{msg.author} results: {song[2]} by {song[1]}")
 
-async def makeRec(amount: int, song: list, rec):
-    distance = []
-    for songs in rec.values:
-        d = 0
-        for col in np.arange(len(rec.columns)):
-            if not col in [0, 1, 2, 3, 10, 13, 16]:
-                d = d + np.absolute(float(song[col]) - float(songs[col]))
-        distance.append(d)
-    rec['distance'] = distance
-    rec = rec.sort_values('distance')
-    columns = ['artist_name', 'track_name', 'track_id']
+        meanslist = [songs[0][0], songs[0][1], songs[0][2], songs[0][3], 0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        # print(meanslist)
+        for i, song in enumerate(songs):
+            # print(i, song)
+            meanslist[4] += song[4]
+            meanslist[5] += song[5]
+            meanslist[6] += song[6]
+            meanslist[7] += song[7]
+            meanslist[8] += song[8]
+            meanslist[9] += song[9]
+            meanslist[10] += song[10]
+            meanslist[11] += song[11]
+            meanslist[12] += song[12]
+            meanslist[13] += song[13]
+            meanslist[14] += song[14]
+            meanslist[15] += song[15]
+            meanslist[16] += song[16]
+            meanslist[17] += song[17]
+        songslen = len(songs)
+        means = ["", meanslist[1], meanslist[2], meanslist[3]]
+        for item in meanslist[4:]:
+            means.append(item / songslen)
+        print(means)
+
+        rec = dataUse[dataUse.track_name.str.lower() != inpSong.lower()]
+        waiting = "Waiting for results...\n"
+        message = await ctx.send(waiting)
     
-    result = rec[columns][:amount]
-    result.drop_duplicates(subset="track_id", keep='first', inplace=True)
-    non_dupes = len(result.index)
-    i = non_dupes
-    while non_dupes < amount:
-        result = rec[columns][:amount+i]
-        result.drop_duplicates(subset="track_id", keep='first', inplace=True)
-        i += 1
-        non_dupes = len(result.index)
-
-    return make_pretty(result)
+        waiting = "Reccomendations for input:\n"
+        await message.edit(content= waiting + await makeRec(10, means, rec))
 
 async def songSearch(sp: spotipy.Spotify, track, artist=""):
     # print(track, artist)
+    # if artist == "":
+    #     input = "track:" + track
+    # else:
+    #     input = "track:" + track + " artist:" + artist
     if artist == "":
-        input = "track:" + track
+        input = track
     else:
-        input = "track:" + track + " artist:" + artist
-    search = sp.search(q=input, type='track', limit=1)
-    # search = sp.search(q=track, type='track', limit=1)
+        input = track + " " + artist
+    search = sp.search(q=input, type='track', limit=5)
+    if len(search['tracks']['items']) <= 0:
+        print("Error")
+        return []
     audio = sp.audio_features([search['tracks']['items'][0]['id']])[0]
     testing = [
         audio['type'], 
@@ -296,7 +221,6 @@ async def playlistSearch(sp: spotipy.Spotify, playlist):
         songslen = len(songs['tracks']['items'])
         means = ["", meanslist[1], meanslist[2], meanslist[3]]
         for item in meanslist[4:]:
-            # print("here")
             means.append(item / songslen)
         # print(frame.mean(numeric_only=True))
 
@@ -305,7 +229,7 @@ async def playlistSearch(sp: spotipy.Spotify, playlist):
 
     return means
 
-def sign_in(author):
+async def sign_in(author):
     cache_handler = spotipy.cache_handler.CacheFileHandler(username=author)
     auth_manager = spotipy.oauth2.SpotifyOAuth(scope='playlist-read-collaborative user-follow-read user-read-recently-played user-library-read',
                                         cache_handler=cache_handler,
@@ -314,6 +238,30 @@ def sign_in(author):
     sp = spotipy.Spotify(auth_manager=auth_manager)
     res = sp.current_user()
     print(res['display_name'])
+
+async def makeRec(amount: int, song: list, rec):
+    distance = []
+    for songs in rec.values:
+        d = 0
+        for col in np.arange(len(rec.columns)):
+            if not col in [0, 1, 2, 3, 10, 13, 16]:
+                d = d + np.absolute(float(song[col]) - float(songs[col]))
+        distance.append(d)
+    rec['distance'] = distance
+    rec = rec.sort_values('distance')
+    columns = ['artist_name', 'track_name', 'track_id']
+    
+    result = rec[columns][:amount]
+    result.drop_duplicates(subset="track_id", keep='first', inplace=True)
+    non_dupes = len(result.index)
+    i = non_dupes
+    while non_dupes < amount:
+        result = rec[columns][:amount+i]
+        result.drop_duplicates(subset="track_id", keep='first', inplace=True)
+        i += 1
+        non_dupes = len(result.index)
+
+    return make_pretty(result)
 
 def make_pretty(result):
     res_str = ""
