488fb56e570be357eb8f2216a772681c3a1bc99a
ejwillis23 <ejwillis@mail.csuchico.edu>
2023-04-25 21:10:10 -0700

    making things nice

--- MusebotSite/spotipyauth/forms.py
+++ MusebotSite/spotipyauth/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.forms import ModelForm, TextInput
 
 class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-    token = forms.CharField(label='Token', max_length=500)
+    your_name = forms.CharField(label='Enter your Discord name and discriminator', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'YourName#0000', 'style': 'width: 300px;', 'class': 'form-control'}))
+    token = forms.CharField(label='Token (do not change)', max_length=500)
 
 class CheckForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+    your_name = forms.CharField(label='Your Dicord name and discriminator', max_length=100)
